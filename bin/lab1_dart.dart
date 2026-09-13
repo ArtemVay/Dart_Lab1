0e9b303b23dfc744d8d169e343553223df7b73ee
ArtemVay <artemvay@inbox.ru>
2026-09-13 16:10:15 +0300

    learn dart functions

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,46 +1,34 @@
 import 'dart:ffi';
-
+String greet(String name) {
+  return "Привет, $name!";
+}
+int square(int x) => x * x;
+double half(double x) => x / 2;
+void describePet({required String name, String species = "кот", int age = 0}) {
+  print("$name - $species, возраст $age");
+}
+String repeat(String text, [int times = 2]) {
+  String result = "";
+  for (int i = 0; i < times; i++) {
+    result += text;
+  }
+  return result;
+}
 void main() {
-  String name = "Артём";
-  int age = 20;
-  double height = 1.75;
-  bool isStudent = true;
-  print(name);
-  print(age);
-  print(height);
-  print(isStudent);
-  print("Привет, $name! Тебе $age лет.");
-  print("Через 5 лет тебе будет, ${age + 5} лет.");
-  print("Рост: $height м, студент: $isStudent");
-  var score = 95;
-  var language = "Dart";
-  print("$language: $score");
-  const String appName = "Lab1";
-  final int startYear = 2026;
-  print ("$appName started in $startYear");
-  String? city = null;
-  if (city != null) {
-    print(
-      city.toUpperCase(),
-    );
-  }
-  print(city?.toUpperCase());
-  String? nickname = null;
-  String display =
-    nickname ?? "Аноним";
-  print(display);
-  List<String> fruits = ["яблоко", "банан", "груша"];
-  fruits.add("апельсин");
-  print(fruits[0]);
-  print(fruits.length);
-  Map<String, dynamic> person = {"name": "Артём", "age": 20};
-  print(person["name"]);
-  person["city"] = "Волжский";
-  Set<int> ids = {1, 2, 3, 2, 1};
-  print(ids);
-  print(ids.length);
-  List<String> fruits2 = ["яблоко", "банан", "груша"];
-  for (var fruit in fruits2) {
-    print(fruit);
-  }
+  print(greet("Артём"));
+  print(greet("Мария"));
+  print(square(5));
+  print(half(7));
+  describePet(name: "Бобик", age: 10);
+  describePet(name: "Стрелка", species: "щенок");
+  print(repeat("ХА"));
+  print(repeat("ХА", 5));
+  List<int> numbers = [3, 1, 4, 1, 5, 9];
+  numbers.sort((a, b) => b - a);
+  print(numbers);
+  List<String> names = ["Артём", "Мария", "Иван"];
+  List<String> upper = names.map((name) => name.toUpperCase()).toList();
+  print(upper);
+  List<String> longNames = names.where((name) => name.length > 4).toList();
+  print(longNames);  
 }
